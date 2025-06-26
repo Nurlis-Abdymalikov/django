7c84d098260e75b712cc1cd985ba943c05cd1f90
Nurlis-Abdymalikov <Nurlisabdymalikov0@gmail.com>
2025-06-26 23:01:36 +0600

    hw7

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ce==s7g@8bh-!&^281uxx45wpz5j8z$a_rem@p_0h!86=ty2)f'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG', 'off') == 'on' else False
 
 ALLOWED_HOSTS = ['*']
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'product',
     'rest_framework.authtoken',
     'users',
+    'drf_yasg',
 ]
 
 REST_FRAMEWORK = {
@@ -84,8 +85,12 @@ WSGI_APPLICATION = 'shop_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('NAME_DB'),
+        'USER': os.environ.get('USER_DB'),
+        'PASSWORD': os.environ.get('PASSWORD_DB'),
+        'HOST': os.environ.get('HOST_DB'),
+        'PORT': os.environ.get('PORT_DB'),
     }
 }
 
--- shop_api/urls.py
+++ shop_api/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from . import swagger
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('api/v1/users/', include('users.urls')),
-
     path('api/v1/', include('product.urls')),
 ]
+urlpatterns += swagger.urlpatterns
